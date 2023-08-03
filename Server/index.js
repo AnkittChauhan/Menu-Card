8b36871e2dccd3cea49d4588cbdd45108164b091
Ankit Chauhan <ankittrajput.4@gmail.com>
2023-08-03 21:42:36 +0530

    update changes

--- Server/index.js
+++ Server/index.js
@@ -8,33 +8,46 @@ app.use(cors())
 
 
 mongoose.connect("mongodb+srv://NeonWolf:R2J6HwBq0s0Zhios@cluster0.qfk8blf.mongodb.net/")
-
-app.get("/getItems" , async ( req , res ) => {
-    itemModel.find().then((err,results) => {
-        if(err){
-            res.json(err)
-        } else{
-            res.json(results)
-        }
+    .then(() => {
+        console.log("Connected to the database");
     })
-})
+    .catch((error) => {
+        console.error("Error connecting to the database:", error);
+    });
 
-app.post("/createItem" , async (req,res) => {
 
-    const items = req.body;
-    const newItems = new itemModel(items);
-    await newItems.save()
+    app.get("/getItems", async (req, res) => {
+        try {
+            const results = await itemModel.find();
+            res.json(results);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+    
 
-    res.json(items)
-})
+    app.post("/createItem", async (req, res) => {
+        try {
+            const items = req.body;
+            const newItems = new itemModel(items);
+            await newItems.save();
+            res.json(items);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+    
 
-app.delete("/deleteItems/:id" , async (req,res) => {
-    const id = req.params.id;
-    await itemModel.findByIdAndDelete(id).exec()
-
-    res.json({message: "ItemRemoved"})
-})
-
+    app.delete("/deleteItems/:id", async (req, res) => {
+        const id = req.params.id;
+        try {
+            await itemModel.findByIdAndDelete(id).exec();
+            res.json({ message: "ItemRemoved" });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+    
 
 
 
