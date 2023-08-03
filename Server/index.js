47083887fc195189f5aa7c02345d18d1319bdba3
Ankit Chauhan <ankittrajput.4@gmail.com>
2023-08-03 16:45:57 +0530

    update Changes

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017")
+
+mongoose.connect(MONGO_URL)
 
 app.get("/getItems" , async ( req , res ) => {
     itemModel.find().then((err,results) => {
